e5540d17cba38a15f481ebceb3b19beed544d756
Garvit Joshi <garvitjoshi9@gmail.com>
2020-07-09 19:49:36 +0530

    Create Structs.cpp

---
+++ Structs.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student
+{
+    int age;
+    char first_name[50];
+    int standard;
+    char last_name[50];
+};
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
