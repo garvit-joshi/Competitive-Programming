9c9443fe5b270736770deed4c5500a8235323697
Garvit Joshi <garvitjoshi9@gmail.com>
2020-09-22 16:22:54 +0530

    Create Sparse Arrays.cpp

---
+++ Sparse Arrays.cpp
@@ -0,0 +1,70 @@
+//Link:https://www.hackerrank.com/challenges/sparse-arrays/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the matchingStrings function below.
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+    vector <int> n;
+    int count=0;
+    for(string q:queries)
+    {
+        for(string s:strings)
+        {
+            if(q==s)
+            {
+                count++;
+            }
+        }
+        n.push_back(count);
+        count =0;
+    }
+    return n;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int strings_count;
+    cin >> strings_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
+    }
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
