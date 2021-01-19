96e5a9cd4f6178e4fd94c5a72dc51264a1e4771d
Garvit Joshi <garvitjoshi9@gmail.com>
2021-01-19 22:49:38 +0530

    Updated Template Code

--- 001 My Template.cpp
+++ 001 My Template.cpp
@@ -2,31 +2,35 @@
 Created by-Garvit Joshi
 garvitjoshi9@gmal.com
 https://github.com/garvit-joshi
+g++ -std=c++11 -O2 -Wall test.cpp -o test
 */
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define ll long long
-#define vpb(v,n) for(int i=0;i<n;i++){int a;cin>>a;v.push_back(a);}
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
 
 void solve() {
-    vector <int> v1;
-    vpb(v1,5);
-    vector<int>::iterator it;
 
-    cout << "vector v1 elements are: ";
-    vshow(v1,it);
-    cout<<"\n";
 }
 
 
 int main() {
 
-    ios_base::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
+
+    /*
+    freopen("input.txt", "r", stdin);   //Input from a file
+    freopen("output.txt", "w", stdout);   //Output to a file
+    */
 
     int tc;
     cin >> tc;
