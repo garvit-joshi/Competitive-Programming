124707a77965309ac4aada13e9c7b98b6cd418c1
Garvit Joshi <garvitjoshi9@gmail.com>
2021-06-21 13:06:33 +0530

    Updated Template

--- 001 My Template.cpp
+++ 001 My Template.cpp
@@ -4,37 +4,55 @@ garvitjoshi9@gmal.com
 https://github.com/garvit-joshi
 g++ -std=c++11 -O2 -Wall test.cpp -o test
 */
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-typedef long long ll;
+#define int long long int
 typedef vector<int> vi;
-typedef pair<int,int> pi;
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i,a,b) for (int i = a; i <= b; i++)
+typedef pair<int, int> pi;
+#define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define EPSILON 1e-9
+// EPSILON=10^(-9), Use:if(abs(a-b) < 1e-9)   //a and b equal
 
-void solve() {
-    
+void debug()
+{
+    cout << "\n";
 }
 
+template <typename First, typename... Strings>
+void debug(First arg, const Strings &...rest)
+{
+    cout << arg << " ";
+    debug(rest...);
+}
 
-int main() {
+int solve()
+{
+
+    return 0;
+}
+
+int32_t main()
+{
 
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    /*
+#ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);   //Input from a file
-    freopen("output.txt", "w", stdout);   //Output to a file
-    */
+    freopen("output.txt", "w", stdout); //Output to a file
+#endif
 
     int tc;
     cin >> tc;
-    while(abs(tc--)) {
+    while (abs(tc--))
+    {
         solve();
     }
     return 0;
